59ea662a4008e058d662f169b3790fa867447108
Nicholas Charriere <nicholascharriere@gmail.com>
2023-07-30 22:37:13 -0700

    Remove next-plausible, try to proxy analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import "./globals.css";
-import PlausibleProvider from "next-plausible";
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import {
@@ -26,25 +25,31 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <PlausibleProvider domain="nicholascharriere.com">
-        <body className={`${roboto.className} bg-bg text-text-500`}>
-          <div className="flex flex-col h-screen justify-between">
-            <header className="h-10 flex justify-end gap-4 items-center mx-6">
-              <a href="https://github.com/nichochar">
-                <GitHubLogoIcon />
-              </a>
-              <a href="https://www.linkedin.com/in/nicholas-charriere/">
-                <LinkedInLogoIcon />
-              </a>
-              <a href="https://twitter.com/nichochar">
-                <TwitterLogoIcon />
-              </a>
-            </header>
-            <main className="px-4"> {children}</main>
-            <footer className="h-10"></footer>
-          </div>
-        </body>
-      </PlausibleProvider>
+      <head>
+        <script
+          defer
+          data-api="/api/event"
+          data-domain="nicholascharriere.com"
+          src="/js/script.js"
+        ></script>
+      </head>
+      <body className={`${roboto.className} bg-bg text-text-500`}>
+        <div className="flex flex-col h-screen justify-between">
+          <header className="h-10 flex justify-end gap-4 items-center mx-6">
+            <a href="https://github.com/nichochar">
+              <GitHubLogoIcon />
+            </a>
+            <a href="https://www.linkedin.com/in/nicholas-charriere/">
+              <LinkedInLogoIcon />
+            </a>
+            <a href="https://twitter.com/nichochar">
+              <TwitterLogoIcon />
+            </a>
+          </header>
+          <main className="px-4"> {children}</main>
+          <footer className="h-10"></footer>
+        </div>
+      </body>
     </html>
   );
 }
