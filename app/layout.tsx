5f9b4c21de201219f2942d9a2d8824795192f8fb
Nicholas Charriere <nicholascharriere@gmail.com>
2023-07-30 22:19:27 -0700

    Add plausible tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import PlausibleProvider from "next-plausible";
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import {
@@ -25,23 +26,25 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${roboto.className} bg-bg text-text-500`}>
-        <div className="flex flex-col h-screen justify-between">
-          <header className="h-10 flex justify-end gap-4 items-center mx-6">
-            <a href="https://github.com/nichochar">
-              <GitHubLogoIcon />
-            </a>
-            <a href="https://www.linkedin.com/in/nicholas-charriere/">
-              <LinkedInLogoIcon />
-            </a>
-            <a href="https://twitter.com/nichochar">
-              <TwitterLogoIcon />
-            </a>
-          </header>
-          <main className="px-4"> {children}</main>
-          <footer className="h-10"></footer>
-        </div>
-      </body>
+      <PlausibleProvider domain="nicholascharriere.com">
+        <body className={`${roboto.className} bg-bg text-text-500`}>
+          <div className="flex flex-col h-screen justify-between">
+            <header className="h-10 flex justify-end gap-4 items-center mx-6">
+              <a href="https://github.com/nichochar">
+                <GitHubLogoIcon />
+              </a>
+              <a href="https://www.linkedin.com/in/nicholas-charriere/">
+                <LinkedInLogoIcon />
+              </a>
+              <a href="https://twitter.com/nichochar">
+                <TwitterLogoIcon />
+              </a>
+            </header>
+            <main className="px-4"> {children}</main>
+            <footer className="h-10"></footer>
+          </div>
+        </body>
+      </PlausibleProvider>
     </html>
   );
 }
