afd8f63791a74db031f8256bf2ca257c168d3d91
Nicholas Charriere <nicholascharriere@gmail.com>
2023-12-27 12:37:14 -0800

    Remove picture, small tweaks

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,11 @@
-import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="container max-w-xl mx-auto">
-      <Image
-        src="/nicholas2.jpg"
-        width="120"
-        height="120"
-        className="rounded-full float-right"
-        alt="Nicholas Charriere"
-      />
-      <div>
       <h1 className="text-3xl font-extrabold my-8">Nicholas Charriere</h1>
       <p className="max-w-xl">
-          Engineer with a passion for creating things. Currently building{" "}
+        Engineer with a passion for creating things. Currently building{" "}
         <a
           className="text-text-700 font-medium underline hover:text-froly-500"
           href="https://axflow.dev"
@@ -29,7 +20,6 @@ export default function Home() {
         </a>
         .
       </p>
-      </div>
       <p className="max-w-xl pt-8 sm:pt-6">
         I recently was a founding engineer at{" "}
         <a
@@ -38,8 +28,7 @@ export default function Home() {
         >
           Privy
         </a>
-        . Before that, I spent 4 years as a
-        staff engineer on the ML platform team at{" "}
+        . Before that I spent 4 years on the ML platform team at{" "}
         <a
           href="https://getcruise.com"
           className="text-text-700 font-medium underline hover:text-froly-500"
@@ -52,19 +41,19 @@ export default function Home() {
           className="text-text-700 font-medium underline hover:text-froly-500"
         >
           Pinterest
-        </a> building real-time systems, and some time at lesser known startups.
+        </a> building real-time ML systems, and a few years working full-stack at lesser known startups.
       </p>
       <p className="max-w-xl pt-8 sm:pt-6">
         I live in California and grew up in France, where I got my
-        engineering Masters from Mines ParisTech.
+        engineering Masters from <a href="https://en.wikipedia.org/wiki/Mines_Paris_%E2%80%93_PSL" className="text-text-700 font-medium underline hover:text-froly-500">Mines Paris</a>.
       </p>
       <div className="pt-8">
-      <Link
-        className="text-text-700 text-xl font-medium underline hover:text-froly-500"
-        href="/projects"
-      >
-        Projects
-      </Link>
+        <Link
+          className="text-text-700 text-xl font-medium underline hover:text-froly-500"
+          href="/projects"
+        >
+          Projects
+        </Link>
       </div>
     </main>
   );
